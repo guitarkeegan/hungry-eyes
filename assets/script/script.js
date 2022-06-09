d262a09fff41c66931b0e4e64e20fd34a3bca7c9
guitarkeegan <keegananglim@gmail.com>
2022-06-09 08:51:45 -0700

    add yelp search fetch

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,2 +1,16 @@
-//testing a merge
-// yes, i can do it!
+// TODO: test yelp fetch api
+// yelp search endpoint https://api.yelp.com/v3/businesses/search
+// yelp business details endpoint https://api.yelp.com/v3/businesses/{id}
+// 
+let userLat = "";
+let userLon = "";
+let searchedCategory = "";
+let limit = 5;
+let yelpEndpoint = `https://api.yelp.com/v3/businesses/search?lat=${userLat}&lon=${userLon}&categories=${searchedCategory}&limit=${limit}`
+fetch(yelpEndpoint, {
+    headers: {
+        Authorization: "Bearer Klnnz8t9NTQXYdSXh_xINM4iG-gO-MuwhkpztrTsDv6qn56ed5zTt2oZM25jBkaVp4zAA4DTJVQg526evOA8_KrmRYFEoYK1cCsH4rbaAXeQTEH1cLns2vOLfgqiYnYx"
+    }
+})
+.then(response => response.json())
+.then(data => console.log(data));
