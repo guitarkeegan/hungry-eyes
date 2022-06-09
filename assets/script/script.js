d99350872ffa1671eb26839e9b7b301079caa1e5
guitarkeegan <keegananglim@gmail.com>
2022-06-09 08:54:09 -0700

    wrap yelp search fetch in function getResaurants

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,11 +6,14 @@ let userLat = "";
 let userLon = "";
 let searchedCategory = "";
 let limit = 5;
-let yelpEndpoint = `https://api.yelp.com/v3/businesses/search?lat=${userLat}&lon=${userLon}&categories=${searchedCategory}&limit=${limit}`
-fetch(yelpEndpoint, {
-    headers: {
-        Authorization: "Bearer Klnnz8t9NTQXYdSXh_xINM4iG-gO-MuwhkpztrTsDv6qn56ed5zTt2oZM25jBkaVp4zAA4DTJVQg526evOA8_KrmRYFEoYK1cCsH4rbaAXeQTEH1cLns2vOLfgqiYnYx"
-    }
-})
-.then(response => response.json())
-.then(data => console.log(data));
+
+function getResaurants(){
+    let yelpEndpoint = `https://api.yelp.com/v3/businesses/search?lat=${userLat}&lon=${userLon}&categories=${searchedCategory}&limit=${limit}`
+    fetch(yelpEndpoint, {
+        headers: {
+            Authorization: "Bearer Klnnz8t9NTQXYdSXh_xINM4iG-gO-MuwhkpztrTsDv6qn56ed5zTt2oZM25jBkaVp4zAA4DTJVQg526evOA8_KrmRYFEoYK1cCsH4rbaAXeQTEH1cLns2vOLfgqiYnYx"
+        }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
