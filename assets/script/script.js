dae6c1663c979dfd20979a5656e292f1487f54a2
guitarkeegan <keegananglim@gmail.com>
2022-06-09 14:39:05 -0700

    add getRandomFoodImages

--- assets/script/script.js
+++ assets/script/script.js
@@ -58,17 +58,16 @@ function printRestaurantDetails(){
 }
 
 function getRandomFoodImages(){
-    // for (let i=0;i<10;i++){
+    for (let i=0;i<6;i++){
         fetch("https://foodish-api.herokuapp.com/api/")
         .then(response=>response.json())
-        .then(data=>randomImageArray.push(data.image));
-
-    // }
-    console.log(randomImageArray[0]);
-    // console.log(typeof randomImageArray[0]);
-    // printRandomFoodImages();
+        .then(data=>{
+            randomImageArray.push(data.image);
+        })
+    }
+    printRandomFoodImages();
 }
 
 function printRandomFoodImages(){
-    // console.log(randomImageArray[2]);
+    console.log(randomImageArray);
 }
