11820a6457a8543db1cea256657a4888bc391491
guitarkeegan <keegananglim@gmail.com>
2022-06-11 08:46:16 -0700

    add responsiveness to more choices button and random images

---
+++ ka-tests/script.js
@@ -0,0 +1,137 @@
+// TODO: test yelp fetch api
+// yelp search endpoint https://api.yelp.com/v3/businesses/search
+// yelp business details endpoint https://api.yelp.com/v3/businesses/{id}
+// foodish endpoint https://foodish-api.herokuapp.com/
+let userLat = "";
+let userLon = "";
+let searchedFoodImage = "";
+let searchedTerm = "";
+let searchedCity = "";
+let resultsLimit = 5;
+let id = "";
+let randomImageArray = [];
+// event listeners
+$(".what-to-eat").on("click", function(){
+    getRandomFoodImages();
+})
+// random food images
+$(".random-img-div").on("click", function(event){
+    const urlArray = event.target.name.split("/");
+    searchedTerm = urlArray[urlArray.length - 2];
+    console.log(searchedTerm);
+    // uncomment bellow to start the yelp search process
+    // getUserLocation()
+});
+$(".choices").on("click", function(){
+    getRandomFoodImages();
+});
+
+function getUserLocation(){
+    // TODO: Make this appear as a modal
+    navigator.geolocation.getCurrentPosition(function(pos) {
+        console.log(pos)
+    })
+    if (userLat && userLon){
+        getRestaurantsByLatLon(userLat, userLon);
+    } else {
+        const city = showModal();
+        getRestuarantsByCity(city);
+    }
+}
+
+function showModal(){
+    return "modal";
+}
+
+
+function getRestaurantsByLatLon(lat, lon){
+    let yelpEndpoint = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?lat=${lat}&lon=${lon}&term=${searchedTerm}&limit=${resultsLimit}`
+    fetch(yelpEndpoint, {
+        headers: {
+            Authorization: "Bearer Klnnz8t9NTQXYdSXh_xINM4iG-gO-MuwhkpztrTsDv6qn56ed5zTt2oZM25jBkaVp4zAA4DTJVQg526evOA8_KrmRYFEoYK1cCsH4rbaAXeQTEH1cLns2vOLfgqiYnYx"
+        }
+    })
+    .then(response => response.json())
+    .then(data => printRestaurantResults(data));
+}
+
+function getRestuarantsByCity(city){
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${city}&term=${searchedTerm}&limit=${resultsLimit}`,{
+        headers: {
+            Authorization: "Bearer Klnnz8t9NTQXYdSXh_xINM4iG-gO-MuwhkpztrTsDv6qn56ed5zTt2oZM25jBkaVp4zAA4DTJVQg526evOA8_KrmRYFEoYK1cCsH4rbaAXeQTEH1cLns2vOLfgqiYnYx"
+        }
+    })
+    .then(response=>response.json())
+    .then(data=>printRestaurantResults(data));
+}
+
+// TODO: decide where to display results
+function printRestaurantResults(data){
+    for (let i=0;i<data.businesses.length();i++){
+        id = data.businesses[i].id;
+        const name = data.businesses[i].name;
+        const rating = data.businesses[i].rating;
+        // searchedFoodImage
+        const imageUrl = data.businesses[i].image_url;
+        const phoneNumber = data.businesses[i].phone;
+    
+    }
+}
+
+
+
+function getRandomFoodImages(){
+    for (let i=0;i<6;i++){
+        fetch("https://foodish-api.herokuapp.com/api/")
+        .then(response=>response.json())
+        .then(data=>{
+            randomImageArray.push(data.image);
+            printRandomFoodImages(i);
+            console.log(randomImageArray);
+        })
+    }
+}
+
+function printRandomFoodImages(imageArrayIndex){
+    $(".choices-button").css({'display': 'block'});
+    $("#choices-button-div").css({'text-align':'center'});
+    $(`#${imageArrayIndex}`).attr('style','')
+    $(`#${imageArrayIndex}`).attr('name', randomImageArray[imageArrayIndex]);
+    $(`#${imageArrayIndex}`).css({'background-image':`url(${randomImageArray[imageArrayIndex]})`,'background-size':'cover','background-position': 'center center', 'width':'100%', 'min-height': '200px'})
+    
+    // $(`#${imageArrayIndex}`).append(`<img class='random-img' name='${randomImageArray[imageArrayIndex]}' src=${randomImageArray[imageArrayIndex]} />`);
+}
+
+
+function getRestaurantDetails(id){
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/${id}`,{
+            headers: {
+                Authorization: "Bearer Klnnz8t9NTQXYdSXh_xINM4iG-gO-MuwhkpztrTsDv6qn56ed5zTt2oZM25jBkaVp4zAA4DTJVQg526evOA8_KrmRYFEoYK1cCsH4rbaAXeQTEH1cLns2vOLfgqiYnYx"
+            }
+        
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+        printRestaurantDetails(data)
+
+    })
+};
+
+function printRestaurantDetails(data){
+    console.log(data)
+    var restaurantName = data.name;
+    var restaurantAddress = data.location.display_address.join("\n")
+    var restaurantPhone = data.display_phone;
+    var restaurantPhoto = data.photos[0]
+   
+
+    
+    //0 is Monday
+}
+
+// TODO: CREATE FUNCTION FOR LOCAL STORAGE
+// TODO: Get from local storage function
+// TODO: display from local storage function
